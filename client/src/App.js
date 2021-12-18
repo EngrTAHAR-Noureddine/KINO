6fa052c0cf2d0d53e2d282bad0613d2f9ffb61f6
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-12-19 00:04:49 +0100

    "add custom carrousel"

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <Paper  style={{width : 'auto', height:'100vh', backgroundColor:"transparent" , border:'none', borderRadius:'none', padding:0, margin:0}}>
       <CustomAppBar/>
+      <Home/>
     </Paper>
   );
 }
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,12 +1,20 @@
-import {Component} from "react";
+import React from 'react';
+import {Box, Paper} from '@mui/material'
+import {Custom_Carrousel} from "../Themes/Elements/Carousel";
 
-class Home extends Component {
-    render() {
-        return (
-            <div>
-                TAHAR
-            </div>
-        )
-    }
+
+function Home() {
+    return (
+        <Box sx={{height:'auto',flexGrow: 1, padding:0,margin:0}}>
+            <Custom_Carrousel/>
+            <Box sx={{height:'100vh',flexGrow: 1, padding:0,margin:0}}>
+
+            </Box>
+        </Box>
+
+    )
 }
+
+
+
 export default Home
