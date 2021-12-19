a314ada2af4af260857b47661c08fab2e22b5bba
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-12-19 16:13:56 +0100

    "add animation to Appbar when scrolling"

--- client/src/Components/CustomAppBar.js
+++ client/src/Components/CustomAppBar.js
@@ -1,84 +1,84 @@
-import {Component} from "react";
 import {AppBar, Box, Button, ButtonGroup, Grid, Toolbar} from '@mui/material';
-import {black_80} from "../Themes/Styles/Color";
-import { MOVIES, SERIES, ANIME, WWE, SIGN_IN, SIGN_UP} from "../Themes/String/String";
-import {TYPOGRAPHY_MENU_XL, TYPOGRAPHY_LOGO} from "../Themes/Styles/Typographies";
+import {ANIME, MOVIES, SERIES, SIGN_IN, SIGN_UP, WWE} from "../Themes/String/String";
+import {TYPOGRAPHY_LOGO, TYPOGRAPHY_MENU_XL} from "../Themes/Styles/Typographies";
 import {MENU_BUTTON_XL} from "../Themes/Elements/Buttons";
 import {ThemeProvider} from "@emotion/react";
 import {SIGN_GROUP_THEME} from "../Themes/Theme/Themes";
 import {SearchBar} from "../Themes/Elements/SearchBar";
 import {MENU_BUTTON} from "../Themes/Elements/IconButton";
+import {ScrollToColor} from './../Themes/Animation/ScrollToColor';
 
-class CustomAppBar extends Component{
 
-    render() {
-        return (
-            <Box sx={{flexGrow: 1}} bgcolor={"transparent"}>
-                    <AppBar position={"fixed"} sx={{backgroundColor:black_80}}>
-                        <Toolbar>
-                            <Grid  container xs={12} >
-                                <Grid  item xs={6} xl={4} bgcolor={'transparent'}
-                                       display={'flex'}
-                                       flexDirection={'row'}
-                                       justifyContent={'left'}>
+function CustomAppBar() {
 
-                                    <MENU_BUTTON/>
-                                    <TYPOGRAPHY_LOGO/>
+    return (
+        <Box sx={{flexGrow: 1}} bgcolor={"transparent"}>
+            <ScrollToColor>
+            <AppBar position={"fixed"} sx={{backgroundColor: 'transparent'}}>
+                <Toolbar>
+                    <Grid container xs={12}>
+                        <Grid item xs={6} xl={4} bgcolor={'transparent'}
+                              display={'flex'}
+                              flexDirection={'row'}
+                              justifyContent={'left'}>
 
-                                </Grid>
-                                <Grid item xs={4} bgcolor={'transparent'}
-                                      display={{xs:'none', xl:'flex'}}
-                                      flexDirection={'column'} alignItems={'center'}
-                                      justifyContent={'center'}>
-                                    <div>
-                                    <MENU_BUTTON_XL item={MOVIES}/>
-                                    <MENU_BUTTON_XL item={SERIES}/>
-                                    <MENU_BUTTON_XL item={ANIME}/>
-                                    <MENU_BUTTON_XL item={WWE}/>
-                                    </div>
-                                </Grid>
-                                <Grid item xs={6} xl={4}
+                            <MENU_BUTTON/>
+                            <TYPOGRAPHY_LOGO/>
+
+                        </Grid>
+                        <Grid item xs={4} bgcolor={'transparent'}
+                              display={{xs: 'none', xl: 'flex'}}
+                              flexDirection={'column'} alignItems={'center'}
+                              justifyContent={'center'}>
+                            <div>
+                                <MENU_BUTTON_XL item={MOVIES}/>
+                                <MENU_BUTTON_XL item={SERIES}/>
+                                <MENU_BUTTON_XL item={ANIME}/>
+                                <MENU_BUTTON_XL item={WWE}/>
+                            </div>
+                        </Grid>
+                        <Grid item xs={6} xl={4}
+                              display={'flex'}
+                              flexDirection={'column'}
+                              justifyContent={'center'}>
+                            <div>
+                                <Grid container xs={12}
                                       display={'flex'}
-                                      flexDirection={'column'}
-                                      justifyContent={'center'}>
-                                    <div>
-                                    <Grid container xs={12}
-                                          display={'flex'}
-                                          flexDirection={'row'}
-                                          justifyContent={'right'}>
+                                      flexDirection={'row'}
+                                      justifyContent={'right'}>
 
-                                        <Grid item xs={12} xl={9}>
-                                           <SearchBar/>
-                                        </Grid>
-
-                                        <Grid item
-                                              lg={3}
-                                              display={{xs:'none', xl:'flex'}}
-                                              flexDirection={'column'}
-                                              justifyContent={'center'}>
-                                           <div>
-                                               <ThemeProvider theme={SIGN_GROUP_THEME}>
-                                                   <ButtonGroup variant="text">
-                                                       <Button sx={{textTransform: 'none'}}>
-                                                           <TYPOGRAPHY_MENU_XL item ={SIGN_IN}/>
-                                                       </Button>
-                                                       <Button sx={{textTransform: 'none'}}>
-                                                           <TYPOGRAPHY_MENU_XL item ={SIGN_UP}/>
-                                                       </Button>
-                                                   </ButtonGroup>
-                                               </ThemeProvider>
-
-                                           </div>
-                                        </Grid>
+                                    <Grid item xs={12} xl={9}>
+                                        <SearchBar/>
                                     </Grid>
-                                    </div>
+
+                                    <Grid item
+                                          lg={3}
+                                          display={{xs: 'none', xl: 'flex'}}
+                                          flexDirection={'column'}
+                                          justifyContent={'center'}>
+                                        <div>
+                                            <ThemeProvider theme={SIGN_GROUP_THEME}>
+                                                <ButtonGroup variant="text">
+                                                    <Button sx={{textTransform: 'none'}}>
+                                                        <TYPOGRAPHY_MENU_XL item={SIGN_IN}/>
+                                                    </Button>
+                                                    <Button sx={{textTransform: 'none'}}>
+                                                        <TYPOGRAPHY_MENU_XL item={SIGN_UP}/>
+                                                    </Button>
+                                                </ButtonGroup>
+                                            </ThemeProvider>
+
+                                        </div>
+                                    </Grid>
                                 </Grid>
-                            </Grid>
-                        </Toolbar>
-                    </AppBar>
-            </Box>
-        )
-    }
+                            </div>
+                        </Grid>
+                    </Grid>
+                </Toolbar>
+            </AppBar>
+            </ScrollToColor>
+        </Box>
+    )
 }
 
 export default CustomAppBar;
--- client/src/Themes/Styles/Color.js
+++ client/src/Themes/Styles/Color.js
@@ -1,6 +1,7 @@
 import {alpha} from "@mui/material";
 
-export const black_80 = '#CC000000';
+export const black_80 = alpha('#000000',0.8);
+export const black_30 = alpha('#000000',0.3);
 export const black_60 = alpha('#000000',0.6);
 export const divider_B1 = '#B1B1B3';
 export const white_100='#FFFFFF';
