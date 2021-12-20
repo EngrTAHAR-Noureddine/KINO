884900559f9d5d144a37620f7c7a7f9bda20cb10
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-12-20 22:06:49 +0100

    "add : prop: name and photo"

--- client/src/Pages/Actor.js
+++ client/src/Pages/Actor.js
@@ -2,13 +2,14 @@ import {Box} from "@mui/material";
 import {Seperator_Bar} from "../Themes/Elements/Seperator_Bar";
 import Result_List from "../Components/Result_List";
 import {BoxPersonPhoto} from "../Components/BoxPersonPhoto";
+import actorPhoto from '../Asset/Actor.jpg';
 
 export default function Actor() {
     const list = [1,2,3,4,5,6];
     return(
         <Box>
             <Seperator_Bar/>
-            <BoxPersonPhoto/>
+            <BoxPersonPhoto name={'James Spader'} photo={actorPhoto}/>
             <Result_List list={list}/>
         </Box>
     )
