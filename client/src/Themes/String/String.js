be2eba90043313963bf2dc76aa65a2351921482f
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-12-20 01:48:01 +0100

    "add : LIST_SHORTCUTS ,FILTER_LIST"

--- client/src/Themes/String/String.js
+++ client/src/Themes/String/String.js
@@ -5,7 +5,40 @@ export const ANIME = 'Anime';
 export const WWE = 'WWE';
 export const SIGN_UP = 'SignUp';
 export const SIGN_IN = 'SignIn';
-
 export const MORE = 'More';
 export const LATEST_ADDITIONS = 'Latest Additions';
 export const MOST_WATCHED = 'Most Watched';
+
+export const FILTER_LIST = [
+
+    {
+        type:'Language',
+        list:['English','Arabic','French','German','Japanese','Chinese','Spanish']
+    },
+    {
+        type:'Status',
+        list:['360px','480px','720px','1080px','2k','4k','8k']
+    },
+    {
+        type:'Quality',
+        list:['BluRay','WEB-DL','DVDRip','HDRip','HDTC','HDTS','HDTV']
+    },
+    {
+        type:'Year',
+        list:['2021','2020','2019','2018','2017','2016','2015']
+    },
+    {
+        type:'Country',
+        list:['USA','England','France','India','Japan','Italy','Russia']
+    },
+    {
+        type:'Category',
+        list:['R','PG-13','TV-14','TV-MA','TV-PG','TV-Y','TV-Y7']
+    },
+    {
+        type:'Type',
+        list:['Action','History','Crime','Drama','War','Sci-fi','Romantic']
+    }
+];
+
+export const LIST_SHORTCUTS = ['List','2021' ,'Trending'];
